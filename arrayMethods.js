e9d420311795eee08e6183b2c020baa7df6ed005
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-09-11 01:14:36 -0600

    arrays completed

--- arrayMethods.js
+++ arrayMethods.js
@@ -17,8 +17,15 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-// CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(function(element) {
+  return element % 2 === 0
+})
+console.log(evenNumbers)
+
+
+ const evenNumbers = mixedNUmbers.filter(element => element % 2 === 0)
+
+console.log(evenNumbers)
 
 
 
@@ -38,8 +45,14 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-// CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+
+const postTaxPrices = prices.map(function(price){
+  return price  * 1.07
+})
+
+const postTaxPrices = prices.map(price => price * 1.07)
+
+console.log(postTaxPrices)
 
 
 
@@ -57,8 +70,13 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation = populations.reduce(function(acc, element){
+  return acc + curr
+})
 
+const totalPopulation = populations.reduce((acc, element) => {a + e})
+
+console.log(totalPopulation)
 
 
 ////////// PROBLEM 4 //////////
@@ -82,8 +100,13 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter(function(element){
+  return element['CP'] > 200
+})
 
+const myStrongest = monstersInYourPocket.filter(element.cp => element.cp > 200)
+
+console.log(myStrongest)
 
 
 ////////// PROBLEM 5 //////////
@@ -96,12 +119,17 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll 
+  need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-// CODE HERE
+const ordersTotal = orders.map(function(total){
+  return total['price'] + total['price'] * total['tax']
+})
 
+const ordersTotal = orders.map(total => total.price + (total.price * total.tax))
 
+console.log(ordersTotal)
 
 ////////// PROBLEM 6 //////////
 
@@ -119,4 +147,10 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method(s) to get the sum of bobsTotal.
 */
 
-// CODE HERE
+const bobsTotal = purchases.filter(function(element){
+  return element.owner === 'Bob'
+  .reduce(function(acc, element){
+    return acc + element.price
+  })
+}, 0)
+
